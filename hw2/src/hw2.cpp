10de66fce356cd068858d6cb5551264ec717864f
William Zhao <williezhao123@gmail.com>
2025-04-04 21:42:37 -0700

    hw2 folder

---
+++ hw2/src/hw2.cpp
@@ -0,0 +1,537 @@
+﻿/*
+  CSCI 420 Computer Graphics, Computer Science, USC
+  Assignment 1: Height Fields with Shaders.
+  C/C++ starter code
+
+  Student username: wzhao029
+*/
+
+#include "openGLHeader.h"
+#include "glutHeader.h"
+#include "openGLMatrix.h"
+#include "imageIO.h"
+#include "pipelineProgram.h"
+#include "vbo.h"
+#include "vao.h"
+#include "Spline.h"
+#include "Camera.h"
+
+#include <OpenGL/OpenGL.h>
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(WIN32) || defined(_WIN32)
+  #ifdef _DEBUG
+    #pragma comment(lib, "glew32d.lib")
+  #else
+    #pragma comment(lib, "glew32.lib")
+  #endif
+#endif
+
+#if defined(WIN32) || defined(_WIN32)
+  char shaderBasePath[1024] = SHADER_BASE_PATH;
+#else
+  char shaderBasePath[1024] = "../openGLHelper";
+#endif
+
+using namespace std;
+
+int mousePos[2]; // x,y screen coordinates of the current mouse position
+
+int leftMouseButton = 0; // 1 if pressed, 0 if not 
+int middleMouseButton = 0; // 1 if pressed, 0 if not
+int rightMouseButton = 0; // 1 if pressed, 0 if not
+
+typedef enum { ROTATE, TRANSLATE, SCALE } CONTROL_STATE;
+CONTROL_STATE controlState = ROTATE;
+
+// Transformations of the terrain.
+float terrainRotate[3] = { 0.0f, 0.0f, 0.0f }; 
+// terrainRotate[0] gives the rotation around x-axis (in degrees)
+// terrainRotate[1] gives the rotation around y-axis (in degrees)
+// terrainRotate[2] gives the rotation around z-axis (in degrees)
+float terrainTranslate[3] = { 0.0f, 0.0f, 0.0f };
+float terrainScale[3] = { 1.0f, 1.0f, 1.0f };
+
+// Width and height of the OpenGL window, in pixels.
+int windowWidth = 1280;
+int windowHeight = 720;
+char windowTitle[512] = "CSCI 420 Homework 2";
+
+uint currentTime = 0;
+uint lastTime = 0;
+float deltaTime = 0;
+
+VAO* pointVAO = nullptr; // VAO for the spline
+VBO* pointVBO = nullptr; // VBO for the spline
+
+int LineEBOIndices = 0;
+GLuint LineEBO; // EBO for the spline
+
+// Number of vertices in the single triangle (starter code).
+
+// CSCI 420 helper classes.
+OpenGLMatrix matrix;
+PipelineProgram * pipelineProgram = nullptr;
+
+const int RestartIndex = 0xFFFF;
+
+// Write a screenshot to the specified filename.
+void saveScreenshot(const char * filename)
+{
+  unsigned char * screenshotData = new unsigned char[windowWidth * windowHeight * 3];
+  glReadPixels(0, 0, windowWidth, windowHeight, GL_RGB, GL_UNSIGNED_BYTE, screenshotData);
+
+  ImageIO screenshotImg(windowWidth, windowHeight, 3, screenshotData);
+
+  if (screenshotImg.save(filename, ImageIO::FORMAT_JPEG) == ImageIO::OK)
+    cout << "File " << filename << " saved successfully." << endl;
+  else cout << "Failed to save file " << filename << '.' << endl;
+
+  delete [] screenshotData;
+}
+
+void idleFunc()
+{
+  // Do some stuff... 
+  // For example, here, you can save the screenshots to disk (to make the animation).
+
+  // Notify GLUT that it should call displayFunc.
+  glutPostRedisplay();
+}
+
+void reshapeFunc(int w, int h)
+{
+  glViewport(0, 0, w, h);
+
+  // When the window has been resized, we need to re-set our projection matrix.
+  matrix.SetMatrixMode(OpenGLMatrix::Projection);
+  matrix.LoadIdentity();
+  // You need to be careful about setting the zNear and zFar. 
+  // Anything closer than zNear, or further than zFar, will be culled.
+  const float zNear = 0.1f;
+  const float zFar = 10000.0f;
+  const float humanFieldOfView = 60.0f;
+  matrix.Perspective(humanFieldOfView, 1.0f * w / h, zNear, zFar);
+}
+
+void mouseMotionDragFunc(int x, int y)
+{
+  // Mouse has moved, and one of the mouse buttons is pressed (dragging).
+
+  // the change in mouse position since the last invocation of this function
+  int mousePosDelta[2] = { x - mousePos[0], y - mousePos[1] };
+
+  switch (controlState)
+  {
+    // translate the terrain
+    case TRANSLATE:
+      if (leftMouseButton)
+      {
+        // control x,y translation via the left mouse button
+        terrainTranslate[0] += mousePosDelta[0] * 0.01f;
+        terrainTranslate[1] -= mousePosDelta[1] * 0.01f;
+      }
+      if (middleMouseButton)
+      {
+        // control z translation via the middle mouse button
+        terrainTranslate[2] += mousePosDelta[1] * 0.01f;
+      }
+      break;
+
+    // rotate the terrain
+    case ROTATE:
+      if (leftMouseButton)
+      {
+        // control x,y rotation via the left mouse button
+        terrainRotate[0] += mousePosDelta[1];
+        terrainRotate[1] += mousePosDelta[0];
+      }
+      if (middleMouseButton)
+      {
+        // control z rotation via the middle mouse button
+        terrainRotate[2] += mousePosDelta[1];
+      }
+      break;
+
+    // scale the terrain
+    case SCALE:
+      if (leftMouseButton)
+      {
+        // control x,y scaling via the left mouse button
+        terrainScale[0] *= 1.0f + mousePosDelta[0] * 0.01f;
+        terrainScale[1] *= 1.0f - mousePosDelta[1] * 0.01f;
+      }
+      if (middleMouseButton)
+      {
+        // control z scaling via the middle mouse button
+        terrainScale[2] *= 1.0f - mousePosDelta[1] * 0.01f;
+      }
+      break;
+  }
+
+  // store the new mouse position
+  mousePos[0] = x;
+  mousePos[1] = y;
+}
+
+void mouseMotionFunc(int x, int y)
+{
+  // Mouse has moved.
+  // Store the new mouse position.
+  mousePos[0] = x;
+  mousePos[1] = y;
+}
+
+void mouseButtonFunc(int button, int state, int x, int y)
+{
+  // A mouse button has has been pressed or depressed.
+
+  // Keep track of the mouse button state, in leftMouseButton, middleMouseButton, rightMouseButton variables.
+  switch (button)
+  {
+    case GLUT_LEFT_BUTTON:
+      leftMouseButton = (state == GLUT_DOWN);
+    break;
+
+    case GLUT_MIDDLE_BUTTON:
+      middleMouseButton = (state == GLUT_DOWN);
+    break;
+
+    case GLUT_RIGHT_BUTTON:
+      rightMouseButton = (state == GLUT_DOWN);
+    break;
+  }
+
+  // Keep track of whether CTRL and SHIFT keys are pressed.
+  switch (glutGetModifiers())
+  {
+    case GLUT_ACTIVE_CTRL:
+      controlState = TRANSLATE;
+    break;
+
+    case GLUT_ACTIVE_SHIFT:
+      controlState = SCALE;
+    break;
+
+    // If CTRL and SHIFT are not pressed, we are in rotate mode.
+    default:
+      controlState = ROTATE;
+    break;
+  }
+
+  if (rightMouseButton)
+  {
+    controlState = TRANSLATE;
+  }
+
+  // Store the new mouse position.
+  mousePos[0] = x;
+  mousePos[1] = y;
+}
+
+void keyboardFunc(unsigned char key, int x, int y)
+{
+  switch (key)
+  {
+    case 27: // ESC key
+      exit(0); // exit the program
+    break;
+    case 'x':
+      // Take a screenshot.
+      saveScreenshot("screenshot.jpg");
+    break;
+  }
+}
+
+int screenshotCounter = 0;
+void displayFunc()
+{
+  currentTime++;
+  deltaTime = 0.016f; // Simulate a fixed time step for the sake of this example.
+//  if (deltaTime > 5)
+//  {
+//    char filename[64];
+//    snprintf(filename, sizeof(filename), "screenshot_%d.jpg", screenshotCounter++);
+//    saveScreenshot(filename);
+//
+//    lastTime = currentTime;
+//  }
+  // This function performs the actual rendering.
+
+  // First, clear the screen.
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glEnable(GL_PRIMITIVE_RESTART);
+  glPrimitiveRestartIndex(RestartIndex);
+
+  updateCameraPosition(deltaTime);
+
+  // Set up the camera position, focus point, and the up vector.
+  matrix.SetMatrixMode(OpenGLMatrix::ModelView);
+  matrix.LoadIdentity();
+  matrix.LookAt(cameraPosition.x, cameraPosition.y, cameraPosition.z,
+                cameraTarget.x, cameraTarget.y, cameraTarget.z,
+                cameraUp.x, cameraUp.y, cameraUp.z);
+
+  // In here, you can do additional modeling on the object, such as performing translations, rotations and scales.
+  matrix.Translate(terrainTranslate[0], terrainTranslate[1], terrainTranslate[2]);
+  matrix.Scale(terrainScale[0], terrainScale[1], terrainScale[2]);
+  matrix.Rotate(terrainRotate[0], 1, 0, 0);
+  matrix.Rotate(terrainRotate[1], 0, 1, 0);
+  matrix.Rotate(terrainRotate[2], 0, 0, 1);
+
+  // Read the current modelview and projection matrices from our helper class.
+  // The matrices are only read here; nothing is actually communicated to OpenGL yet.
+  float modelViewMatrix[16];
+  matrix.SetMatrixMode(OpenGLMatrix::ModelView);
+  matrix.GetMatrix(modelViewMatrix);
+
+  float projectionMatrix[16];
+  matrix.SetMatrixMode(OpenGLMatrix::Projection);
+  matrix.GetMatrix(projectionMatrix);
+
+  // Upload the modelview and projection matrices to the GPU. Note that these are "uniform" variables.
+  // Important: these matrices must be uploaded to *all* pipeline programs used.
+  // In hw2, there is only one pipeline program, but in hw2 there will be several of them.
+  // In such a case, you must separately upload to *each* pipeline program.
+  // Important: do not make a typo in the variable name below; otherwise, the program will malfunction.
+  pipelineProgram->SetUniformVariableMatrix4fv("modelViewMatrix", GL_FALSE, modelViewMatrix);
+  pipelineProgram->SetUniformVariableMatrix4fv("projectionMatrix", GL_FALSE, projectionMatrix);
+
+  drawGeometry();
+  // Swap the double-buffers.
+  glutSwapBuffers();
+}
+
+void initScene(int argc, char *argv[])
+{
+  // Set the background color.
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black color.
+
+  // Enable z-buffering (i.e., hidden surface removal using the z-buffer algorithm).
+  glEnable(GL_DEPTH_TEST);
+
+  // Create a pipeline program. This operation must be performed BEFORE we initialize any VAOs.
+  // A pipeline program contains our shaders. Different pipeline programs may contain different shaders.
+  // In this homework, we only have one set of shaders, and therefore, there is only one pipeline program.
+  // In hw2, we will need to shade different objects with different shaders, and therefore, we will have
+  // several pipeline programs (e.g., one for the rails, one for the ground/sky, etc.).
+  pipelineProgram = new PipelineProgram(); // Load and set up the pipeline program, including its shaders.
+  // Load and set up the pipeline program, including its shaders.
+  if (pipelineProgram->BuildShadersFromFiles(shaderBasePath, "vertexShader.glsl", "fragmentShader.glsl") != 0)
+  {
+    cout << "Failed to build the pipeline program." << endl;
+    throw 1;
+  } 
+  cout << "Successfully built the pipeline program." << endl;
+    
+  // Bind the pipeline program that we just created. 
+  // The purpose of binding a pipeline program is to activate the shaders that it contains, i.e.,
+  // any object rendered from that point on, will use those shaders.
+  // When the application starts, no pipeline program is bound, which means that rendering is not set up.
+  // So, at some point (such as below), we need to bind a pipeline program.
+  // From that point on, exactly one pipeline program is bound at any moment of time.
+  pipelineProgram->Bind();
+
+  generateSplineVAO();
+
+  // Check for any OpenGL errors.
+  std::cout << "GL error status is: " << glGetError() << std::endl;
+}
+
+void generateSplineVAO() {
+
+  vector<float> colors;
+  initSpline();
+  initColor(colors, splinePoints.size()/3);
+  pointVAO = new VAO();
+  pointVBO = new VBO(splinePoints.size(), 3, &splinePoints[0], GL_STATIC_DRAW);
+
+  pointVAO->ConnectPipelineProgramAndVBOAndShaderVariable(pipelineProgram, pointVBO, "position");
+  pointVAO->ConnectPipelineProgramAndVBOAndShaderVariable(pipelineProgram, pointVBO, "color");
+
+  vector<int> lineIndices;
+  for (int i = 0; i < splinePoints.size()/3; i++) {
+    lineIndices.push_back(i);
+  }
+  lineIndices.push_back(RestartIndex);
+  LineEBOIndices = lineIndices.size();
+  glGenBuffers(1, &LineEBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, LineEBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, LineEBOIndices * sizeof(int), &lineIndices[0], GL_STATIC_DRAW);
+
+}
+
+void drawGeometry() {
+  pointVAO->Bind();
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, LineEBO);
+  glDrawElements(GL_LINE_STRIP, LineEBOIndices, GL_UNSIGNED_INT, 0);
+}
+
+
+void initColor(vector<float>& vect, int numVertices) {
+  for (int i = 0; i < numVertices; i++) {
+    vect.push_back(1.0f);
+    vect.push_back(1.0f);
+    vect.push_back(1.0f);
+    vect.push_back(1.0f);
+  }
+}
+
+int initTexture(const char * imageFilename, GLuint textureHandle)
+{
+  // Read the texture image.
+  ImageIO img;
+  ImageIO::fileFormatType imgFormat;
+  ImageIO::errorType err = img.load(imageFilename, &imgFormat);
+
+  if (err != ImageIO::OK) 
+  {
+    printf("Loading texture from %s failed.\n", imageFilename);
+    return -1;
+  }
+
+  // Check that the number of bytes is a multiple of 4.
+  if (img.getWidth() * img.getBytesPerPixel() % 4) 
+  {
+    printf("Error (%s): The width*numChannels in the loaded image must be a multiple of 4.\n", imageFilename);
+    return -1;
+  }
+
+  // Allocate space for an array of pixels.
+  int width = img.getWidth();
+  int height = img.getHeight();
+  unsigned char * pixelsRGBA = new unsigned char[4 * width * height]; // we will use 4 bytes per pixel, i.e., RGBA
+
+  // Fill the pixelsRGBA array with the image pixels.
+  memset(pixelsRGBA, 0, 4 * width * height); // set all bytes to 0
+  for (int h = 0; h < height; h++)
+    for (int w = 0; w < width; w++) 
+    {
+      // assign some default byte values (for the case where img.getBytesPerPixel() < 4)
+      pixelsRGBA[4 * (h * width + w) + 0] = 0; // red
+      pixelsRGBA[4 * (h * width + w) + 1] = 0; // green
+      pixelsRGBA[4 * (h * width + w) + 2] = 0; // blue
+      pixelsRGBA[4 * (h * width + w) + 3] = 255; // alpha channel; fully opaque
+
+      // set the RGBA channels, based on the loaded image
+      int numChannels = img.getBytesPerPixel();
+      for (int c = 0; c < numChannels; c++) // only set as many channels as are available in the loaded image; the rest get the default value
+        pixelsRGBA[4 * (h * width + w) + c] = img.getPixel(w, h, c);
+    }
+
+  // Bind the texture.
+  glBindTexture(GL_TEXTURE_2D, textureHandle);
+
+  // Initialize the texture.
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelsRGBA);
+
+  // Generate the mipmaps for this texture.
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  // Set the texture parameters.
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // Query support for anisotropic texture filtering.
+  GLfloat fLargest;
+  glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &fLargest);
+  printf("Max available anisotropic samples: %f\n", fLargest);
+  // Set anisotropic texture filtering.
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 0.5f * fLargest);
+
+  // Query for any errors.
+  GLenum errCode = glGetError();
+  if (errCode != 0) 
+  {
+    printf("Texture initialization error. Error code: %d.\n", errCode);
+    return -1;
+  }
+  
+  // De-allocate the pixel array -- it is no longer needed.
+  delete [] pixelsRGBA;
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {  
+    printf ("Usage: %s <spline file>\n", argv[0]);
+    exit(0);
+  }
+
+  // Load spline from the provided filename.
+  loadSpline(argv[1]);
+
+  printf("Loaded spline with %d control point(s).\n", spline.numControlPoints);
+
+  cout << "Initializing GLUT..." << endl;
+  glutInit(&argc,argv);
+
+  cout << "Initializing OpenGL..." << endl;
+
+  #ifdef __APPLE__
+    glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_STENCIL);
+  #else
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_STENCIL);
+  #endif
+
+  glutInitWindowSize(windowWidth, windowHeight);
+  glutInitWindowPosition(0, 0);  
+  glutCreateWindow(windowTitle);
+
+  cout << "OpenGL Version: " << glGetString(GL_VERSION) << endl;
+  cout << "OpenGL Renderer: " << glGetString(GL_RENDERER) << endl;
+  cout << "Shading Language Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+
+  #ifdef __APPLE__
+    // This is needed on recent Mac OS X versions to correctly display the window.
+    glutReshapeWindow(windowWidth - 1, windowHeight - 1);
+  #endif
+
+  // Tells GLUT to use a particular display function to redraw.
+  glutDisplayFunc(displayFunc);
+  // Perform animation inside idleFunc.
+  glutIdleFunc(idleFunc);
+  // callback for mouse drags
+  glutMotionFunc(mouseMotionDragFunc);
+  // callback for idle mouse movement
+  glutPassiveMotionFunc(mouseMotionFunc);
+  // callback for mouse button changes
+  glutMouseFunc(mouseButtonFunc);
+  // callback for resizing the window
+  glutReshapeFunc(reshapeFunc);
+  // callback for pressing the keys on the keyboard
+  glutKeyboardFunc(keyboardFunc);
+
+  // init glew
+  #ifdef __APPLE__
+    // nothing is needed on Apple
+  #else
+    // Windows, Linux
+    GLint result = glewInit();
+    if (result != GLEW_OK)
+    {
+      cout << "error: " << glewGetErrorString(result) << endl;
+      exit(EXIT_FAILURE);
+    }
+  #endif
+
+  // Perform the initialization.
+  initScene(argc, argv);
+
+  // Sink forever into the GLUT loop.
+  glutMainLoop();
+}
+
